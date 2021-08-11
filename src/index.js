b5b385437496f991c5a850306e9337294f9f589a
Zaikou Eugene <evgeniikas9@gmail.com>
2021-08-11 14:25:52 +0300

    feat: add tast array-problem

--- src/index.js
+++ src/index.js
@@ -1,12 +1,48 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array === undefined || array.length == 0) {
+    return 0;
+  } else {
+    let minNumber = array[0];
+    for( let i = 1; i < array.length; i++) {
+      if(array[i] < minNumber ) {
+        minNumber = array[i];
+      }
+    }
+    return minNumber;
+  }
 }
 
+
+
+
 exports.max = function max (array) {
-  return 0;
+  if(array === undefined || array.length == 0) {
+    return 0;
+  } else {
+    let maxNumber = array[0];
+    for( let i = 1; i < array.length; i++) {
+      if(array[i] > maxNumber ) {
+        maxNumber = array[i];
+      }
+    }
+    return maxNumber;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array === undefined || array.length == 0) {
+    return 0;
+  } else {
+    let count = 0;
+    let result = 0;
+    for( let i = 0; i < array.length; i++) {
+      result += array[i];
+      count++;
+    }
+
+
+
+    return result / count;
+  }
 }
